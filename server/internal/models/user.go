9c483925391e823b3981052fa8ab7252da5aef96
Clint George <clintbgeorge@gmail.com>
2025-10-19 18:52:55 +0530

    Modified user model
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- server/internal/models/user.go
+++ server/internal/models/user.go
@@ -1,8 +1,13 @@
 package models
 
+import "time" 
+
 type User struct {
-	ID       uint   `gorm:"primaryKey"`
-	Email    string `gorm:"unique"`
-	Password string
-	Balance  float64
+	ID        uint      `gorm:"primaryKey"`
+	Email     string    `gorm:"unique"`
+	UserName  string
+	Password  string
+	Balance   float64
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"` 
 }
